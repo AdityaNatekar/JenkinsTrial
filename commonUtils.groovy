0d68c902dfd326014757dbd5606ad95c68761c0e
AdityaNatekar <80022733+AdityaNatekar@users.noreply.github.com>
2024-07-15 00:48:53 +0900

    Create commonUtils.groovy

---
+++ commonUtils.groovy
@@ -0,0 +1,8 @@
+// CommonUtils.groovy
+
+def notify(String message) {
+    // Example: Notify based on the message
+    echo "Notification: ${message}"
+}
+
+// Add more reusable methods as needed...
