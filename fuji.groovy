caf5ecc07f07e4519d00d1ff437d7f6ddf39308b
AdityaNatekar <80022733+AdityaNatekar@users.noreply.github.com>
2024-07-15 01:42:39 +0900

    Create fuji.groovy

---
+++ fuji.groovy
@@ -0,0 +1,14 @@
+// fuji.groovy
+
+class Fuji {
+    
+    void say() {
+        echo "I'm fuji aoi!"
+    }
+
+    void sing(String title) {
+        echo "Sing the ${title}"
+    }
+    
+}
+return new Fuji()
