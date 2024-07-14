94e4b28598fc80e037183f186a8fd37ff482e997
AdityaNatekar <80022733+AdityaNatekar@users.noreply.github.com>
2024-07-15 01:48:57 +0900

    Update fuji.groovy

--- fuji.groovy
+++ fuji.groovy
@@ -1,14 +1,9 @@
-// fuji.groovy
-
 class Fuji {
-    
     void say() {
-        echo "I'm fuji aoi!"
+        println "I'm fuji aoi!"
     }
 
     void sing(String title) {
-        echo "Sing the ${title}"
+        println "Sing the ${title}"
     }
-    
 }
-return new Fuji()
