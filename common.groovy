7c52be7c2f0b91d8919793e3e2a7a56d6edd6a80
AdityaNatekar <80022733+AdityaNatekar@users.noreply.github.com>
2024-07-15 17:09:12 +0900

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -1,9 +1,16 @@
-class Helper implements Serializable {
+import groovy.transform.Field
 
-    static def printShellExecution(String command) {
-        def result = sh(script: command, returnStdout: true).trim()
-        echo "Shell command executed: ${command}"
-        echo "Result: ${result}"
+class Helper {
+    transient def steps // Marking as transient to avoid serialization
+
+    Helper(steps) {
+        this.steps = steps
+    }
+
+    def printShellExecution(String command) {
+        def result = steps.sh(script: command, returnStdout: true).trim()
+        steps.echo "Shell command executed: ${command}"
+        steps.echo "Result: ${result}"
         return result
     }
 }
