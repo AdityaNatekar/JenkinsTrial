282390e86abc314668dd7bfd1a3430378eb75682
AdityaNatekar <80022733+AdityaNatekar@users.noreply.github.com>
2024-08-08 23:07:18 +0900

    Update BuildManager.groovy

--- BuildManager.groovy
+++ BuildManager.groovy
@@ -3,38 +3,38 @@ import groovy.json.JsonOutput
 
 class BuildManager {
     boolean forceBuild
-    Map builtJson
 
-    // BuildManager(boolean forceBuild, String builtJsonString) {
-    //     this.forceBuild = forceBuild
-    //     this.builtJson = new JsonSlurper().parseText(builtJsonString)
-    // }
+
+    BuildManager(boolean forceBuild) {
+        this.forceBuild = forceBuild
+
+    }
 
     // Function to remove '#number' suffix from all component values
-    private Map removeNumberSuffixFromComponents(Map json) {
-        if (json.containsKey('component')) {
-            def updatedComponent = json.component.collect { componentList ->
-                componentList.collect { component ->
-                    def name = component[1]
-                    if (name.contains('#')) {
-                        // Remove '#number' from the component name
-                        name = name.replaceAll(/#\d+$/, '')
-                    }
-                    [component[0], name]
-                }
-            }
-            json.component = updatedComponent
-        }
-        return json
-    }
+    // private Map removeNumberSuffixFromComponents(Map json) {
+    //     if (json.containsKey('component')) {
+    //         def updatedComponent = json.component.collect { componentList ->
+    //             componentList.collect { component ->
+    //                 def name = component[1]
+    //                 if (name.contains('#')) {
+    //                     // Remove '#number' from the component name
+    //                     name = name.replaceAll(/#\d+$/, '')
+    //                 }
+    //                 [component[0], name]
+    //             }
+    //         }
+    //         json.component = updatedComponent
+    //     }
+    //     return json
+    // }
 
     // Function to decide whether to reuse all components based on the forceBuild flag
     Map buildJudgement() {
         if (forceBuild) {
-            return removeNumberSuffixFromComponents(builtJson)
+            return true
         }
-        return builtJson
+        return false
     }
 }
 
-return new BuildManager()
+// return new BuildManager()
