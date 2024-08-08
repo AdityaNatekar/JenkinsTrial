bae0a23573a3043c906d9a35924fb1c099af4afa
AdityaNatekar <80022733+AdityaNatekar@users.noreply.github.com>
2024-08-08 23:30:16 +0900

    Update BuildManager.groovy

--- BuildManager.groovy
+++ BuildManager.groovy
@@ -2,38 +2,39 @@ import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
 class BuildManager {
-
-
+    boolean forceBuild
+    Map builtJson
 
     BuildManager() {
-    
 
     }
 
     // Function to remove '#number' suffix from all component values
-    // private Map removeNumberSuffixFromComponents(Map json) {
-    //     if (json.containsKey('component')) {
-    //         def updatedComponent = json.component.collect { componentList ->
-    //             componentList.collect { component ->
-    //                 def name = component[1]
-    //                 if (name.contains('#')) {
-    //                     // Remove '#number' from the component name
-    //                     name = name.replaceAll(/#\d+$/, '')
-    //                 }
-    //                 [component[0], name]
-    //             }
-    //         }
-    //         json.component = updatedComponent
-    //     }
-    //     return json
-    // }
+    private Map removeNumberSuffixFromComponents(Map json) {
+        if (json.containsKey('component')) {
+            def updatedComponent = json.component.collect { componentList ->
+                componentList.collect { component ->
+                    def name = component[1]
+                    if (name.contains('#')) {
+                        // Remove '#number' from the component name
+                        name = name.replaceAll(/#\d+$/, '')
+                    }
+                    [component[0], name]
+                }
+            }
+            json.component = updatedComponent
+        }
+        return json
+    }
 
     // Function to decide whether to reuse all components based on the forceBuild flag
-    boolean buildJudgement(boolean forcebuild) {
-        if (forcebuild) {
-            return true
+    Map buildJudgement(boolean forceBuild, String builtJsonString) {
+        builtJson = new JsonSlurper().parseText(builtJsonString)
+        if (forceBuild) {
+            return removeNumberSuffixFromComponents(builtJson)
         }
-        return false
+        return builtJson
     }
 }
- return new BuildManager()
+
+return BuildManager()
